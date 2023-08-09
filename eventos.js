c51df15ba9576ffb80f740c01efc49853d6807da
Sofia Blum <sofy_blum@hotmail.com>
2023-08-08 22:49:06 -0300

    div con evento en js

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+let divElement = document.querySelector("div");
+
+divElement.addEventListener("click", () => {
+    alert("¡Hola desde el div!")
+});
+});
+
+
