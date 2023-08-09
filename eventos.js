39ca380d0d36465d36865000a8ac9a143875306f
Sofia Blum <sofy_blum@hotmail.com>
2023-08-08 23:47:05 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
+
 let divElement = document.querySelector("div");
+
+let btn = document.getElementById("btn");
+    
+btn.addEventListener("click", (e) =>  {
+    alert("¡Hola!");
+    e.stopPropagation();
+});
 
 divElement.addEventListener("click", () => {
     alert("¡Hola desde el div!")
